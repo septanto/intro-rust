37296ce70b3869a2b40dfd20e21e392148013c40
septanto <s.s.adi.nugroho@gmail.com>
2024-11-15 13:42:57 +0700

    implicit return

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,25 @@ struct Deck {
     cards: Vec<String>,
 }
 
+impl Deck{
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+    
+        let mut cards = vec![];
+    
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+}
+
 fn main() {
-    let deck = Deck { cards: vec![]};
-    println!("Here's your deck: {:?}", deck);
+    let deck = Deck::new();
+    println!("Here's your deck: {:#?}", deck);
 }
